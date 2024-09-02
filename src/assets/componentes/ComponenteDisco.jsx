ba019c5a434f552c29d4a28315e1f9cef94d70a8
Danna <dannalubel29@gmail.com>
2024-09-02 11:45:16 -0300

    crear componente disco e implementado con dammy data

---
+++ src/assets/componentes/ComponenteDisco.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const ComponenteDisco = () => {
+    const dummyData =[
+        {
+            "id": 1,
+            "artista": "artista1",
+            "titulo": "El mejor disco",
+            "duracion": 30,
+            "cod": "abc123"
+        },
+        {
+            "id": 2,
+            "artista": "artista2",
+            "titulo": "disco no tan bueno",
+            "duracion": 30,
+            "cod": "bca123"
+        },
+        {
+            "id": 3,
+            "artista": "artista3",
+            "titulo": "disco mas o menos bueno",
+            "duracion": 30,
+            "cod": "aaa123"
+        },
+        {
+            "id": 4,
+            "artista": "jinjer",
+            "titulo": "flower",
+            "duracion": 30,
+            "cod": "asd123"
+        },
+        {
+            "id": 5,
+            "artista": "nostra morte",
+            "titulo": "percefone",
+            "duracion": 30,
+            "cod": "dsa321"
+        },
+        {
+            "id": 6,
+            "artista": "type o negative",
+            "titulo": "cinnamon girl",
+            "duracion": 30,
+            "cod": "awd153"
+        }
+    ]
+
+
+  return (
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Artista</th>
+                    <th>Titulo</th>
+                    <th>Duracion</th>
+                    <th>Codigo</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    dummyData.map(disco => 
+                        <tr key={disco.id}>
+                            <td>{disco.id}</td>
+                            <td>{disco.artista}</td>
+                            <td>{disco.titulo}</td>
+                            <td>{disco.duracion}</td>
+                            <td>{disco.cod}</td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ComponenteDisco
